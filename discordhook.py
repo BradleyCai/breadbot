9cd21de681a97b3e62055dd493fe08b1e5bb3afe
BradleyCai <bradcai24@gmail.com>
2017-07-12 15:37:26 -0700

    Inlined post content

--- discordhook.py
+++ discordhook.py
@@ -2,13 +2,10 @@ import requests
 
 # Posts an image with an optional caption
 def post_img(url, img, text=None):
-    payload = {'content': text}
-    files = {'file': img}
-
     if text == None:
-        return requests.post(url, files=files)
+        return requests.post(url, files={'file': img})
     else:
-        return requests.post(url, data=payload, files=files)
+        return requests.post(url, data={'content': text}, files={'file': img})
 
 # Posts text
 def post_text(url, text):
