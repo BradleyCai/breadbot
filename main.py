2b44770627d3ed8cf3535cb1556bc49754a5e9b0
BradleyCai <bradcai24@gmail.com>
2017-07-12 15:58:01 -0700

    Reorganized functions for finding source, and make unknown pixiv page clearer

--- main.py
+++ main.py
@@ -17,20 +17,23 @@ def getimage(unused_path, used_path):
 
     return imgname
 
-# Has two returns. If a pixiv source is found, returns the pixiv url and the page
-# If no source is found, returns None and None
+# Tries to find source of the image
+# For now, only supports images saved from pixiv
 def getsource(imgname):
+    return get_pixiv_source(imgname)
+
+def get_pixiv_source(imgname):
     pixiv_url = 'https://www.pixiv.net/member_illust.php?mode=medium&illust_id='
 
+    # Match computer saved links (ex: 28022143_p0.jpg)
     m = re.match(r'(\d{7,9})_?p(\d{1,3})', imgname)
-
     if m:
         return pixiv_url + m.group(1), m.group(2)
-    else:
-        m = re.match(r'illust_(\d{7,9})_\d{8}_\d{6}', imgname)
 
-        if m:
-            return pixiv_url + m.group(1), "0"
+    # Match mobile saved links (ex: illust_51503473_20170620_040102.jpg)
+    m = re.match(r'illust_(\d{7,9})_\d{8}_\d{6}', imgname)
+    if m:
+        return pixiv_url + m.group(1), '?' # page is not saved in link name
 
     return None, None
 
@@ -41,7 +44,9 @@ def format_source(source_url, page, imgname):
     else:
         res = '**Source: **<' + source_url + '>'
 
-        if page != '0':
+        if page == '?':
+            res += '\n**Page: ** Page unknown"
+        elif page != '0':
             res += '\n**Page: **' + str(int(page) + 1)
 
     res += '\n**Original filename: **`' + imgname + '`'
