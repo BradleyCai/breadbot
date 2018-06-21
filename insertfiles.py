9ffbae82c18edc4cbe04b6d8f9111b18195b5d3a
BradleyCai <bradcai24@gmail.com>
2018-06-20 18:23:33 -0700

    Ignore inserfiles script

--- insertfiles.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import filelister
-import subprocess
-
-subprocess.run(['tar', '-xzf', 'queue.tar.gz'], stdout=subprocess.PIPE)
-subprocess.run(['mv', 'home/bread/winhome/pictures/anime/queue', 'bots'])
-filelister.insertfiles('bots/a2c.json')
-
